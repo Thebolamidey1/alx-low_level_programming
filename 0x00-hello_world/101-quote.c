7d700c4f01cd4116c69530fb83cc4db57bde436a
Omobolanle <bolanletolani93@gmail.com>
2022-09-08 15:53:23 -0700

    quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - entry point
+ * main - Entry point
  *
- * Return= always 1 (success)
+ * Return= Always 1 (success)
  */
 int main(void)
 {
-printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
