363c408ea59a95038ebd1c87756bb700c45fe5a2
Omobolanle <bolanletolani93@gmail.com>
2022-09-09 08:58:07 -0700

    alphaBETS

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char f
+
+	f = 'a';
+	while (f <= 'z')
+	{
+		putchar(f);
+		f++;
+	}
+	f = 'A';
+	while (f <= 'Z')
+	{
+		putchar(f);
+		f++;
+	}
+	putchar('\n');
+	return (0);
+}
