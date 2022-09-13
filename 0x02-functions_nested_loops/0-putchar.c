327212fa3a1ea00ab6204be568e0297904ef154c
Omobolanle <bolanletolani93@gmail.com>
2022-09-13 07:23:40 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,15 +7,15 @@
  */
 int main(void)
 {
-	char p[] = "_putchar\n";
-	
-	char v;
-	v = p[];
-	
-	while (v = p[])
-	{
-		_putchar(v);
-		v++;
-	}
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
+
 	return (0);
 }
