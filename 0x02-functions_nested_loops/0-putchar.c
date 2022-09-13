651364a8c567124ab459c0a92b81afbe2a2408ac
Omobolanle <bolanletolani93@gmail.com>
2022-09-13 08:02:39 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,19 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * main - print _putchar
  *
  * Return: 0 (success)
  */
 int main(void)
 {
-	_putchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
-	_putchar("\n");
+	char *weput = "_putchar";
 
+	while (*weput)
+	{
+		_putchar(*weput);
+		weput++;
+	}
+	_putchar("\n");
 	return (0);
 }
