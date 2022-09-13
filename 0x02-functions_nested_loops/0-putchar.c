d3abca728118b1aef0bf371153fab422a0574391
Omobolanle <bolanletolani93@gmail.com>
2022-09-13 05:19:11 -0700

    my commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <main.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char p[] = "_putchar\n";
+	
+	char v;
+	v = p[];
+	
+	while (v = p[])
+	{
+		_putchar(v);
+		v++;
+	}
+	return (0);
+}
